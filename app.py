7698401db88bed44ffac0bd8ad5b09540f410232
Mannendri Olivares <mannendriolivares@mannendris-mbp.home>
2021-08-22 00:44:23 -0400

    Completed home, login, and sign up screens

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, current_app as app
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,37 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # form = LoginForm()
+
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.username.data).first()
+    #     if user:
+    #         if check_password_hash(user.password, form.password.data):
+    #             login_user(user, remember=form.remember.data)
+    #             return redirect(url_for('dashboard'))
+
+    #     return '<h1>Invalid username or password</h1>'
+    #     #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    # form = RegisterForm()
+
+    # if form.validate_on_submit():
+    #     hashed_password = generate_password_hash(form.password.data, method='sha256')
+    #     new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+    #     db.session.add(new_user)
+    #     db.session.commit()
+
+    #     return '<h1>New user has been created!</h1>'
+    #     #return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,18 +1,92 @@
 let map;
 
 function initMap() {
-    const codeNextHarlem = { lat: 40.826297630189195, lng: -73.94860685803098 };
-    const codeNextChelsea = { lat: 40.741511656087575, lng: -74.00254440220917 };
+    // const codeNextHarlem = { lat: 40.826297630189195, lng: -73.94860685803098 };
+    // const codeNextChelsea = { lat: 40.741511656087575, lng: -74.00254440220917 };
+    // const nypl = { lat: 40.75359558129044, lng: -73.98191480220885}
+    const codeNextNewYork = { lat: 40.75959120191965, lng: -73.99207367721833};
+    const codeNextOakland = { lat: 37.77624876854818, lng: -122.2242399292719 }; 
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 40.75959120191965, lng: -73.99207367721833 },
-        zoom: 8,
+        zoom: 3,
     });
-    const markerOne = new google.maps.Marker({
-        position: codeNextHarlem,
+    const markerNewYork = new google.maps.Marker({
+        icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_blue2.png',
+        position: codeNextNewYork,
         map: map,
       });
-    const markerTwo = new google.maps.Marker({
-        position: codeNextChelsea,
+    const markerOakland = new google.maps.Marker({
+        icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_blue1.png',
+        position: codeNextOakland,
         map: map,
       });
+
+    // const markerOne = new google.maps.Marker({
+    //     position: codeNextHarlem,
+    //     map: map,
+    //   });
+    // const markerTwo = new google.maps.Marker({
+    //     position: codeNextChelsea,
+    //     map: map,
+    //   });
+    // const markerThree = new google.maps.Marker({
+    //     position: nypl,
+    //     map: map,
+    //   });
+
+    // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      if (!place.geometry || !place.geometry.location) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
